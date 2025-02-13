31a18423a4ac6c379b5364e1b755f2b5d73bdadb
Filip Szymanski <arren13@gmail.com>
2025-02-13 15:59:18 +0100

    prettier

--- demo-bank/tests/login.spec.ts
+++ demo-bank/tests/login.spec.ts
@@ -4,37 +4,37 @@ test.describe('User login to Demobank', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
   });
-  
+
   test('login with correct credentials', async ({ page }) => {
     //Arrange
     const userId = 'tester12';
     const userPwd = 'pwd12345';
     const expectedUser = 'Jan Demobankowy';
-    
+
     //Act
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPwd);
     await page.getByTestId('login-button').click();
-    
+
     //Assert
     await expect(page.getByTestId('user-name')).toHaveText(expectedUser);
   });
-  
+
   test('unsuccessful login with short login', async ({ page }) => {
     //Arrange
     const incorrectUserId = '1234567';
     const expectedErrorMsg = 'identyfikator ma min. 8 znaków';
-    
+
     //Act
     await page.getByTestId('login-input').fill(incorrectUserId);
     await page.getByTestId('password-input').click();
-    
+
     //Assert
     await expect(page.getByTestId('error-login-id')).toHaveText(
       expectedErrorMsg,
     );
   });
-  
+
   test('unsuccessful login with short pwd', async ({ page }) => {
     //Arrange
     const userId = 'tester12';
